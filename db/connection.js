3bacd4dc4e42266b9b9caedfb248639f23c6a517
Michel Lizardo <michel@Michels-MacBook-Pro.local>
2022-04-14 15:52:37 -0400

    fixed the mongodb link

--- db/connection.js
+++ db/connection.js
@@ -2,7 +2,7 @@ import mongoose from "mongoose";
 import chalk from "chalk";
 
 const MONGODB_URI =
-  process.env.PROD_MONGODB || "mongodb://127.0.0.1:27017/";
+  process.env.PROD_MONGODB || "mongodb://127.0.0.1:27017/google-archive";
 
 mongoose.set("returnOriginal", false);
 
