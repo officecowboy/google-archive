cdbec38ed03b124f81ed6a8e15f9769da5e2fc5a
Scott Diekema <scott.diekema@gmail.com>
2022-04-18 11:19:06 -0400

    add home screen

--- src/screens/Home.jsx
+++ src/screens/Home.jsx
@@ -1,7 +1,14 @@
-import React from 'react'
+import React from 'react';
+import Logo from '../components/header/Logo';
+import Search from '../components/header/Search';
 
 export default function Home() {
   return (
-    <div>Home</div>
+    <div className="home-search-container">
+      <div className="home-search">
+        <Logo />
+        <Search />
+      </div>
+    </div>
   )
 }
