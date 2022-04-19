279aaba0f83646d59e5b66ce3c003051e20b37f3
Scott Diekema <scott.diekema@gmail.com>
2022-04-18 21:56:38 -0400

    logo image link

--- src/components/header/Logo.jsx
+++ src/components/header/Logo.jsx
@@ -1,10 +1,9 @@
 import React from 'react'
-import { Link } from 'react-router-dom'
 
 export default function Logo() {
   return (
     <div className="logo">
-      <img className="logo-image" alt="" src={require("../../assets/logos/GoogleLogo2000to5-5-2010.png")}></img>
-    </div>
+      <img className="logo-image" alt="" src={require("../../assets/logos/GoogleLogo2000to5-5-2010.png")} />
+    </div >
   )
 }
