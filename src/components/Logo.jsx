60dcf5f25b0b76615f7d747818d6bb57bdc06c26
Scott Diekema <scott.diekema@gmail.com>
2022-04-19 17:57:43 -0400

    alien logo

--- src/components/Logo.jsx
+++ src/components/Logo.jsx
@@ -4,8 +4,8 @@ import { Link } from 'react-router-dom';
 export default function Logo() {
   return (
     <div className="logo">
-      <Link to="/">
-        <img className="logo-image" alt="" src={require("../assets/logos/GoogleLogo2000to5-5-2010.png")} />
+      <Link to="/"><img className="logo-image" alt="" src='//www.google.com/logos/2000/doodle_alien3.gif' />
+
       </Link>
     </div >
   )
