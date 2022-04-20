78e754211a3b61311c37eea7ddf582d757a8ba2e
Scott Diekema <scott.diekema@gmail.com>
2022-04-20 11:10:26 -0400

    update about text formatting

--- src/screens/Home.jsx
+++ src/screens/Home.jsx
@@ -25,7 +25,7 @@ export default function Home({ doodles, handleChange, dateEntry, setDate, dateSo
           <div className="about">
             <p className="lead-about">Did you know that Google was originally named <a href="http://blogoscoped.com/archive/2007-12-28-n47.html" target="blank">Backrub</a>?<br /> Here on <a href="https://backrub.wiki/" target="blank">backrub.wiki</a>, we trace the history of technology from the new millennium to the present:</p>
             <ol>
-              <li>Enter a date from January 1, 2000 - April 1, 2022 to view <a href="https://www.nytimes.com/section/technology" target="blank">The New York Times</a> articles published in the Technology Section on that date.</li>
+              <li>Enter a date from January 1, 2000 - April 1, 2022 to view all articles published in <a href="https://www.nytimes.com/section/technology" target="blank">The New York Times</a> Technology Section on that day.</li>
               <li>Click on the Google logo to load a random <a href="https://www.google.com/doodles" target="blank">Google Doodle</a> from the past 20 years.</li>
               <li>Add your own memories on the search page!</li>
             </ol>
